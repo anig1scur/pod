2654237a8416b90199792b031f66bb37ec3f57f0
anig1scur <anigiscur@gmail.com>
2024-07-04 02:03:15 +0800

    ådd about

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,101 @@
+import React, { useEffect } from 'react';
+
+interface DialogProps {
+  content: React.ReactNode;
+  header?: React.ReactNode;
+  headerText?: string;
+  onOk: () => void;
+  onCancel: () => void;
+  okText?: string;
+  cancelText?: string;
+  showBottom?: boolean;
+  isOpen: boolean;
+}
+
+const Dialog: React.FC<DialogProps> = ({
+  content,
+  headerText,
+  header,
+  onOk,
+  onCancel,
+  okText = 'OK',
+  cancelText = 'Cancel',
+  showBottom = true,
+  isOpen
+}) => {
+
+  if (!isOpen) {
+    return null;
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onCancel();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+
+  return (
+    <div
+      className={ `fixed inset-0 z-20 overflow-y-auto transition-opacity duration-300 ease-in-out ${ isOpen ? 'opacity-100' : 'opacity-0' }` }
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="flex items-end justify-center min-h-screen pt-4 px-2 pb-20 text-center sm:block sm:p-0" >
+        <div onClick={ onCancel } className={ `fixed inset-0 bg-gray-900 bg-opacity-50 transition-opacity ${ isOpen ? 'opacity-100' : 'opacity-0'
+          }` } aria-hidden="true"></div>
+
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <div
+          className={ `inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-[40vw] sm:w-full ${ isOpen
+            ? 'opacity-100 translate-y-0 sm:scale-100'
+            : 'opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+            }` }
+        >
+          <div className="bg-white px-2 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:text-left">{
+                header ? header : <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">{ headerText }</h3>
+              }
+                {
+                  !showBottom && (
+                    <div className='absolute top-4 right-6 cursor-pointer font-bold text-2xl' onClick={ onCancel }>x</div>)
+                }
+                <div className="mt-2">
+                  { content }
+                </div>
+              </div>
+            </div>
+          </div>
+          { showBottom && (
+            <div className="bg-gray-50 px-2 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <button
+                type="button"
+                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={ onOk }
+              >
+                { okText }
+              </button>
+              <button
+                type="button"
+                className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={ onCancel }
+              >
+                { cancelText }
+              </button>
+            </div>
+          ) }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dialog;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,80 @@
+import React, { useState } from 'react';
 import Logo from '@/components/Logo';
-import React from 'react';
+import Dialog from '@/components/Dialog';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+
+const About = () => {
+  return (
+    <div className='about'>
+      <h3>
+        1. POD ! 是什么？
+      </h3>
+      <p>
+        Pod 是一个用于英语学习的前端应用，包括音频 / 阅读 / 听写 / 词汇模块。
+      </p>
+      <h3>
+        2. POD ! 的数据来源是什么？
+      </h3>
+      <p>
+        现在 Pod 的数据来源于 <a href='https://www.bbc.co.uk/learningenglish/english/features/6-minute-english' target='_blank'>BBC 6 minute English</a> 和 <a href='https://www.scientificamerican.com/podcasts/' target='_blank'>科学美国人</a>
+        的部分播客，后续会看情况增加更多。
+      </p>
+      <h3>
+        2. POD ! 的词汇表有哪些
+      </h3>
+      <p>
+        Pod 的词汇表取自网络，包括
+
+        <a href='https://github.com/anig1scur/CEFR-Vocabulary-List/' target='_blank'>CEFR</a>
+        ,
+        <a href='https://www.eapfoundation.com/vocab/academic/awllists/' target='_blank'>AWL</a>
+        ,
+        <a href='https://www.vocabulary.com/lists/128536' target='_blank'>GRE 5000</a>
+        ,
+        <a href='https://r.piggy.lol/pod/assets/pdf/manhattan_prep_1000_gre_words_.pdf' target='_blank'>GRE Manhattan Prep 1000</a>
+        ,
+        以及
+        <a href='https://quizlet.com/tw/211687200/mason-gre-2000-flash-cards/' target='_blank'>
+          GRE Mason 2000
+        </a>
+      </p>
+      <h3>
+        3. 我发现了错误或者有改进的建议，应该怎么做？
+      </h3>
+      <p>
+        点击右上角的 <a href='https://github.com/anig1scur/pod/' className='github bg-black mx-1'></a> 进入此项目的 GitHub 仓库， 点击 issues Tab 提交你的问题或者建议。
+      </p>
+    </div>
+  )
+}
 
 const Header = () => {
+  const [showDialog, setShowDialog] = useState(false);
   return (
     <header>
-        <Logo text="POD!" />
-        <a href="https://github.com/anig1scur/pod" target='_blank' className='github' />
+      <Logo text="POD!" />
+      <div className='flex justify-center items-center text-zinc-800'>
+        <>
+          <FontAwesomeIcon
+            title='about'
+            className='cursor-pointer w-6 h-6 mx-4'
+            icon={ faQuestionCircle }
+            onClick={ () => setShowDialog(true) }
+          />
+          {
+            <Dialog
+              isOpen={ showDialog }
+              header={ <div className='text-3xl mb-5'>关于 <strong>POD !</strong> </div> }
+              content={ <About /> }
+              showBottom={ false }
+              onOk={ () => setShowDialog(false) }
+              onCancel={ () => setShowDialog(false) }
+            />
+          }
+        </>
+        <a title='source code' href="https://github.com/anig1scur/pod" target='_blank' className='github bg-zinc-800' />
+      </div>
     </header>
   );
 }
